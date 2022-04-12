7f944234c980a7ffe2314e760f3ae3c633a186ad
Jhezuann <pjhezuann@gmail.com>
2022-04-12 14:07:47 -0430

    subir proyecto

---
+++ historias.py
@@ -0,0 +1,18 @@
+#Historia 1
+A1 = input("Ingrese un Adjetivo: ")
+V1 = input("Ingrese un Verbo: ")
+V2 = input("Ingrese un Verbo: ")
+S1 = input("Ingrese un Sustantivo(Plural): ")
+
+historia = f"¡Programar es tan {A1}! Siempre me emociona porque me encanta {V1} problemas. ¡Aprende a {V2} en python y alcanza tus {S1}!"
+
+print(historia)
+
+#Historia 2
+name = input("Enter your name: ")
+nationality = input("Enter country of birth: ")
+hobby = input("enter a hobby: ")
+
+history = f"Hello, my name is {name}, I am from {nationality} and in my free time I dedicate myself to {hobby}"
+
+print(history)
